3183b8cc18ba9f8c21f08b557fa61da8679c451e
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-04-16 15:26:15 -0700

    Started to create page top button
    Added html and css for a top button that appears when scrolling down the page. Started to write jQuery to animate the scroll to top.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var btn = $('#topButton');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: 0
+  }, '300');
+});
+
 $(function () {
   $("#menu-toggle").click(function (e) {
     e.preventDefault();
